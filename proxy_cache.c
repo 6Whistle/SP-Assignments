d3d1cfb364bd296646e566c3e534aeac98af5afc
6Whistle <camjun0506@icloud.com>
2022-04-03 08:20:38 -0700

    1-2 ver.3

--- proxy_cache.c
+++ proxy_cache.c
@@ -45,7 +45,6 @@ char *sha1_hash(char *input_url, char *hashed_url) {
 }
 
 
-
 ////////////////////////////////////////////////////////////////////////////////
 // getHomeDir								      //
 //============================================================================//
@@ -61,35 +60,65 @@ char *getHomeDir(char *home){
   return home;
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Check_Exist_File							      //
+//============================================================================//
+// Input : char *path -> Directory's path,				      //
+//    char *file_name -> file name,				     	      //
+//    char is_exist_file -> if directory is exist : 1, else : 0, 	      //
+// Output : int -> if path/file is exist : return 1, else : return 0	      //
+// Purpose : Checking path/file is exist				      //
+////////////////////////////////////////////////////////////////////////////////
+
 int Check_Exist_File(char* path, char *file_name, int is_exist_file){
-  if(is_exist_file == 0){
+  if(is_exist_file == 0){         //if Directory isn't exist, return 0
     return 0;
   }
 
-  DIR *dir = opendir(path);
+  DIR *dir = opendir(path);       //Open path directoy
   struct dirent *d;
 
-  while(d = readdir(dir))
-    if(strcmp(d->d_name, file_name) == 0){
+  while(d = readdir(dir))         //Check path directory
+    if(strcmp(d->d_name, file_name) == 0){    //if file name is exist, return 1
       closedir(dir);
       return 1;
     }
   
-  closedir(dir);
+  closedir(dir);        //if file name isn't  exist, return 0
   return 0;
 }
 
-void Write_Log_File(FILE *log_file, char *input_url, char *hashed_url_dir, char *hashed_url_file, int is_exist_file){
-  time_t now;
-  struct tm *ltp;
 
-  time(&now);
+////////////////////////////////////////////////////////////////////////////////
+// Write_Log_File							      //
+//============================================================================//
+// Input : File *log_file -> opened file pointer,			      //
+//    char *input_url -> input URL,				     	      //
+//    char *hashed_url_dir -> hashed directory name,			      //
+//    char *hashed_url_file -> hashed file name,			      //
+//    char is_exist_file -> if directory/file is exist : 1, else : 0, 	      //
+//    int *hit -> count of hit,						      //
+//    int *miss -> count of miss,					      //
+// Output : void						       	      //
+// Purpose : Write that is input URL hit or miss in log file		      //
+////////////////////////////////////////////////////////////////////////////////
+
+void Write_Log_File(FILE *log_file, char *input_url, char *hashed_url_dir, char *hashed_url_file, int is_exist_file, int *hit, int *miss){
+  time_t now;       //Current time
+  struct tm *ltp;   //Local time
+
+  time(&now);       //Save Current time
   ltp = localtime(&now);
-  if(is_exist_file == 0)
-    fprintf(log_file, "[MISS]%s-[%d/%d/%d, %02d:%02d:%02d]\n", input_url, ltp->tm_year, ltp->tm_mon, ltp->tm_mday, ltp->tm_hour, ltp->tm_min, ltp->tm_sec);
-  else{
+
+  if(is_exist_file == 0){   //if file isn't exist, write miss state at logfile.txt
+    fprintf(log_file, "[MISS]%s-[%d/%d/%d, %02d:%02d:%02d]\n", input_url, ltp->tm_year+1900, ltp->tm_mon, ltp->tm_mday, ltp->tm_hour, ltp->tm_min, ltp->tm_sec);
+    (*miss)++;    //miss count
+  }
+  else{   //if file is exist, write hit state at logfile.txt
     fprintf(log_file, "[HIT]%s/%s-[%d/%d/%d, %02d:%02d:%02d]\n", hashed_url_dir, hashed_url_file, ltp->tm_year, ltp->tm_mon, ltp->tm_mday, ltp->tm_hour, ltp->tm_min, ltp->tm_sec);
     fprintf(log_file, "[HIT]%s\n", input_url);
+    (*hit)++;     //hit count
   }
 }
 
@@ -105,38 +134,44 @@ void main(void){
   char *dir_div;		//Seperate point of hashed URL name
   char temp_dir[100];		//Path is used when it makes directory or file
   
-  int is_exist_file;
+  int is_exist_file;  //State directory/file is exist
+  int hit = 0;      //Count hit
+  int miss = 0;     //Count miss
 
   FILE *temp_file;		//Using when make a empty file
   FILE *log_file;     //Using when write log file
 
-  
+  time_t start_time;  //Program start time
+  time_t end_time;    //Program end time
 
-  getHomeDir(home_dir);		//Store ~/cache at home_dir
+  time(&start_time);    //Check Start time
+
+  getHomeDir(home_dir);		//Find ~ directory
+
   strcpy(cache_dir, home_dir);
-  strcat(cache_dir, "/cache");
+  strcat(cache_dir, "/cache");    //cache_dir = ~/cache
+
   strcpy(log_dir, home_dir);
-  strcat(log_dir, "/logfile");
+  strcat(log_dir, "/logfile");    //log_dir = ~/logfile
 
   umask(000);			//Directory's permission can be drwxrwxrwx
   mkdir(cache_dir, 0777);	//make ~/cache Directory
   mkdir(log_dir, 0777);   //make ~/logfile Directory
   
   strcpy(temp_dir, log_dir);
-  strcat(temp_dir, "/logfile.txt");
+  strcat(temp_dir, "/logfile.txt");   //Open ~/logfile/logfile.txt (read, write, append mode)
   log_file = fopen(temp_dir, "a+");
-  fseek(log_file, 0, SEEK_END);
+  fseek(log_file, 0, SEEK_END);       //file pointer is in the end of file
 
   while(1){
     printf("input url> ");	//get URL
     scanf("%s", input);
-    if(strcmp(input, "bye") == 0){	//if it's 'bye' command, end program
-      fseek(log_file, 0, SEEK_SET);
-      while(fgets(output, 100, log_file) != NULL){
-        printf("%s", output);
-      }
+    if(strcmp(input, "bye") == 0){	//if it's 'bye' command, write log at logfile.txt and end program
+      
+      time(&end_time);      //check end time
+      fprintf(log_file, "[Terminated] run time: %d sec #request hit : %d, miss : %d\n", (int)(end_time-start_time), hit, miss);   //write execute time, hit, miss in logfile.txt
       fclose(log_file);
-      return;
+      return;         //end program
     }
       
     is_exist_file = 1;
@@ -148,15 +183,14 @@ void main(void){
     strcpy(temp_dir, cache_dir);		//Make directory ~/cache/Directory name (permission : rwxrwxrwx)
     strcat(temp_dir, "/");
     strcat(temp_dir, first_dir);
-
-    if(mkdir(temp_dir, 0777) == 0)
+    if(mkdir(temp_dir, 0777) == 0)    //Directory isn't already exist, is_exist_file is 0 
       is_exist_file = 0;
-    is_exist_file = Check_Exist_File(temp_dir, dir_div, is_exist_file);
-    Write_Log_File(log_file, input, first_dir, dir_div, is_exist_file);
+
+    is_exist_file = Check_Exist_File(temp_dir, dir_div, is_exist_file);   //Check ~/cache/Directory name/File name is exist
+    Write_Log_File(log_file, input, first_dir, dir_div, is_exist_file, &hit, &miss);    //Write the state(hit or miss) in logfile.txt
 
     strcat(temp_dir, "/");		//Make empty file ~/cache/Directory name/File name (premission : rwxrwxrwx)
     strcat(temp_dir, dir_div);
-    
     temp_file = fopen(temp_dir, "w");
     fclose(temp_file);
   }
